0b39505524a8b6796ba0c08084668f477ce28cc1
Said Gadzhiev <saga080700@gmail.com>
2023-12-11 20:38:44 -0500

    server setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
-
+import { useEffect } from "react";
 function App() {
+  useEffect(() => {
+    fetch('/bacon')
+      .then((res) => res.json())
+      .then((data) => console.log(data.data));
+  }, []);
   return (
     <div>
       <header>
